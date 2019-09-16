99a32cbdac0e54a97a072ab46253b5bc05056a6a
haelmorn <haelmorn@gmail.com>
2019-09-17 01:27:50 +0200

    tweaks

--- f1hub/constructorstandings/schema.py
+++ f1hub/constructorstandings/schema.py
@@ -10,7 +10,7 @@ class ConstructorstandingType(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
-    constructorstandings = graphene.List(ConstructorstandingType,round = graphene.String(), year = graphene.Int())
+    constructorstandings = graphene.List(ConstructorstandingType, round = graphene.String(), year = graphene.Int())
 
     def resolve_constructorstandings(self, info, year = 2019, round = "last", **kwargs):
         data = Constructorstanding.objects.all()
@@ -18,7 +18,7 @@ class Query(graphene.ObjectType):
             data = data.filter(raceId_id__year=year)
         if round == "last":
             max_value = data.filter(raceId_id__year=year).order_by('-raceId_id__round').first()
-            data = data.filter(raceId_id__year=year).filter(raceId_id__round=max_value)
+            data = data.filter(raceId_id__year=year).filter(raceId_id__round=str(max_value))
         elif round != "last":
             data = data.filter(raceId_id__round=round)
         return data
