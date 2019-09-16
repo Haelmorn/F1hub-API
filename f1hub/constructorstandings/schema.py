104c3dc8ed32ffa76e9b28a2fc0b41a5954e1a06
haelmorn <haelmorn@gmail.com>
2019-09-17 01:11:18 +0200

    Added a sensible default for constructorresult round

--- f1hub/constructorstandings/schema.py
+++ f1hub/constructorstandings/schema.py
@@ -12,12 +12,12 @@ class ConstructorstandingType(DjangoObjectType):
 class Query(graphene.ObjectType):
     constructorstandings = graphene.List(ConstructorstandingType,round = graphene.String(), year = graphene.Int())
 
-    def resolve_constructorstandings(self, info, year = None, round = None, **kwargs):
+    def resolve_constructorstandings(self, info, year = 2019, round = "last", **kwargs):
         data = Constructorstanding.objects.all()
         if year:
             data = data.filter(raceId_id__year=year)
         if round == "last":
-            data = data.latest('raceId_id__round')
+            data = data.filter(raceId_id__year=year).latest('raceId_id__round')
         elif round != "last":
             data = data.filter(raceId_id__round=round)
         return data
