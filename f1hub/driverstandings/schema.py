852ac83fb7bc87ecf56ab19ac33293b75809d695
haelmorn <haelmorn@gmail.com>
2019-09-17 12:05:31 +0200

    Added filters to driverstandings

--- f1hub/driverstandings/schema.py
+++ f1hub/driverstandings/schema.py
@@ -1,5 +1,6 @@
 import graphene
 from graphene_django import DjangoObjectType
+from django.db.models import Max
 
 from .models import Driverstanding
 
@@ -10,7 +11,15 @@ class DriverstandingType(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
-    driverstandings = graphene.List(DriverstandingType)
+    driverstandings = graphene.List(DriverstandingType, round = graphene.String(), year = graphene.Int())
 
-    def resolve_driverstandings(self, info, **kwargs):
-        return Driverstanding.objects.all()
+    def resolve_driverstandings(self, info, year = 2019, round = "last", **kwargs):
+        data = Driverstanding.objects.all()
+        if year:
+            data = data.filter(raceId_id__year=year)
+        if round == "last":
+            max_value = data.aggregate(Max('raceId_id__round'))['raceId_id__round__max']
+            data = data.filter(raceId_id__year=year).filter(raceId_id__round=str(max_value))
+        elif round != "last":
+            data = data.filter(raceId_id__round=round)
+        return data
