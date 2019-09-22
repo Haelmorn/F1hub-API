a42f6f44ad5ef28b9c6bf2123abd75a0aac052e5
haelmorn <haelmorn@gmail.com>
2019-09-22 12:24:57 +0200

    Added 'last' filter to results table

--- f1hub/results/schema.py
+++ f1hub/results/schema.py
@@ -1,5 +1,6 @@
 import graphene
 from graphene_django import DjangoObjectType
+from django.db.models import Max
 
 from .models import Result
 
@@ -10,7 +11,7 @@ class ResultType(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
-    results = graphene.List(ResultType, raceId=graphene.Int(), constructor=graphene.String(), year=graphene.Int(), round=graphene.Int(), driver = graphene.String(), limit=graphene.Int(), grid=graphene.Int())
+    results = graphene.List(ResultType, raceId=graphene.Int(), constructor=graphene.String(), year=graphene.Int(), round=graphene.String(), driver = graphene.String(), limit=graphene.Int(), grid=graphene.Int())
 
     def resolve_results(self, info, raceId=None, constructor=None, year=None, round=None, driver=None, limit=999, grid=None, **kwargs):
         data = Result.objects.all()
@@ -20,7 +21,10 @@ class Query(graphene.ObjectType):
             data = data.filter(constructorId_id__constructorRef__icontains=constructor)
         if year:
             data = data.filter(raceId_id__year=year)
-        if round:
+        if round == "last":
+            max_value = data.aggregate(Max('raceId_id__round'))['raceId_id__round__max']
+            data = data.filter(raceId_id__year=year).filter(raceId_id__round=str(max_value))
+        elif round != "last":
             data = data.filter(raceId_id__round=round)
         if driver:
             data = data.filter(driverId_id__driverRef__icontains=driver)
